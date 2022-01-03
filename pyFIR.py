43d0f7e63433547383b3d9ffd5dc99c50ab9dfc9
Elephant ~ <r.davicarvalho@gmail.com>
2022-01-03 20:19:44 -0300

    bug fixes+ broken UPOLS

--- pyFIR.py
+++ pyFIR.py
@@ -28,6 +28,7 @@ SOFTWARE.
 '''
 
 import numpy as np
+from numpy.core.fromnumeric import partition
 from numpy.fft import fft, ifft
 
 
@@ -38,40 +39,44 @@ class FIRfilter():
         h: optional call, initializes the impulse response to be convolved
     '''
     
-    def __init__(self, method="overlap-save", B=512, h=None):    
+    def __init__(self, method="overlap-save", B=512, h=None, partition=512):    
         self.method = method
-        self.B = B
-        self.NFFT = None
-        self.num_input_blocks = None
-        self.y = None
-        self.flagIRchanged = True
-        self.Ny = None
-        self.stored_h = h
+        self.B = B                # block size (audio input len, which will also be the output size)
+        self.NFFT = None          # fft/ifft size
+        self.flagIRchanged = True # check if the IR changed or it's still the same
+        self.stored_h = h         # save IR for comparison next frame (optional input)
         self.left_overs = np.zeros((B,)) # remaining samples from last ifft (OLA)
-               
-    
+        self.partition = partition # partition size (UPOLS)       
+        
+        
     def len(x):
         return max(np.shape(x))
                 
 
-    def pad_zeros_to(self, x, new_length):
+    def pad_the_end(self, x, new_length):
         output = np.zeros((new_length,))
         output[:x.shape[0]] = x
         return output
 
+    
+    def pad_beginning(self, x, padding):
+        new_length = x.shape[0] + padding
+        output = np.zeros((new_length,))
+        output[-x.shape[0]:] = x
+        return output
+    
 
     def next_power_of_2(self, n):
         return 1 << (int(np.log2(n - 1)) + 1)
 
 
     def fft_conv(self,x, h):
-        # Calculate fft
+        # Calculate x fft
         X = fft(x, self.NFFT)
+        # Calculate h fft
         if self.flagIRchanged: # store the IR fft
             self.H = fft(h, self.NFFT)
             self.flagIRchanged = False
-        # Perform convolution
-        # Go bacK to time domain       
         return ifft(X * self.H).real 
 
 
@@ -84,12 +89,14 @@ class FIRfilter():
         # Fast convolution
         y = self.fft_conv(x, h)
 
-        # Overlap-Add the partial convolution result
+        # Overlap-Add the partial convolution result        
         out = y[:self.B] + self.left_overs[:self.B]
         
         self.left_overs = np.delete(self.left_overs, [range(0,self.B)], 0)    
         pad = len(y[self.B+1:])
-        self.left_overs = self.pad_zeros_to(self.left_overs,pad) + y[self.B+1:]        
+        self.left_overs = self.pad_the_end(self.left_overs,pad) + y[self.B+1:] 
+        if len(self.left_overs) < self.B: # fix size for next loop (if needed)
+            self.left_overs = self.pad_the_end(self.left_overs, self.B)      
         return out
 
 
@@ -102,22 +109,74 @@ class FIRfilter():
             self.OLS_buffer = np.zeros(shape=(self.NFFT,))        
         
         # Sliding window of the input
-        self.OLS_buffer = np.roll(self.OLS_buffer, -self.B)
-        self.OLS_buffer[self.B+1:2*self.B+1] = x
+        self.OLS_buffer = np.roll(self.OLS_buffer, -self.B) #previous contents are shifted B samples to the left
+        self.OLS_buffer[self.B:2*self.B] = x #next length-B input block is stored rightmost
 
         # Fast convolution
-        y = self.fft_conv(self.OLS_buffer, h)        
+        y = self.fft_conv(self.OLS_buffer, h)     
         return y[-self.B:]
 
-
+    
+    def UPOLS(self, x,h):
+        '''(generalized) Uniformly Partitioned Overlap-Save
+            - generalized means that you can pick the partition size
+        '''
+        if self.NFFT is None: # only run on on initial call 
+            Nx = max(x.shape)
+            Nh = max(h.shape)
+            # self.NFFT = self.next_power_of_2(Nx + Nh - 1) 
+            self.NFFT = 2*self.B
+            L_partit = self.partition # sub filter length (partition length)
+            self.P = int(np.ceil(Nh/L_partit)) # number of partitions done
+            # Input buffer
+            self.input_buffer = np.zeros(shape=(self.NFFT,))                
+            # variables for delay line
+            # nB = np.zeros((P,)) # block multiple delay nB
+            # dm = np.zeros((P,)) # remainder delay (added at the beginning)
+            kmax = np.lcm(L_partit, self.B)/L_partit
+            dmax = self.B - np.gcd(L_partit, self.B)
+            self.FDL = np.zeros((self.P, self.NFFT), dtype='complex_')  # delay line 
+            self.H = np.zeros((self.P, self.NFFT),dtype='complex_') # partitioned filters (freq domain)
+            # (1) split original filter into P length-L sub filters  
+            for m, ii in enumerate(range(0, Nh, L_partit)):
+                try:
+                    h_partit = h[ii : ii+L_partit]
+                except:
+                    h_partit = self.pad_the_end(h[ii:], L_partit)
+                    
+                # (2) incorporate "remainder delays"
+                if m < kmax:
+                    dm = np.mod(m*L_partit, self.B)
+                else:
+                    dm = dmax          
+                h_pad = self.pad_the_end(self.pad_beginning(h_partit, dm), self.NFFT)  
+                self.H[m,:] = fft(h_pad)                
+                
+        # Sliding window of the input
+        self.input_buffer = np.roll(self.input_buffer, -self.B) #previous contents are shifted B samples to the left
+        self.input_buffer[self.B:2*self.B] = x #next length-B input block is stored rightmost     
+        ### Stream ###
+        self.FDL = np.roll(self.FDL, -1, axis=0) # shift the FDL to create space for current input  
+        self.FDL[0,:] = fft(self.input_buffer) # add current buffer to the first FDL slot
+        # convo
+        out = np.sum(ifft(np.multiply(self.FDL, self.H)).real, axis=0)
+        return out[-self.B:]
+                  
+                  
+                  
+# %% Main     
     def process(self, x, h):       
         # check if the impulse response h has changed
         if np.all(h != self.stored_h):
             self.flagIRchanged = True
             self.stored_h = h
+        else:
+            h = self.stored_h             
             
         # convolve
-        if self.method == 'overlap-save':
+        if self.method=='overlap-save' or self.method=='OLS':
             return self.OLS(x, h)
-        if self.method == 'overlap-add':
+        elif self.method=='overlap-add' or self.method=='OLA':
             return self.OLA(x, h)
+        elif self.method=='UPOLS':
+            return self.UPOLS(x, h)
