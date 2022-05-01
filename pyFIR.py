aa4bcc83dfa358934a76463a1ccdb62757370e1e
Elephant ~ <r.davicarvalho@gmail.com>
2022-05-01 19:04:10 +0100

    minor fix

--- pyFIR.py
+++ pyFIR.py
@@ -39,23 +39,21 @@ class FIRfilter():
         B: defines the block length
         h: optional call, initializes the impulse response to be convolved
     '''
-    
+
     def __init__(self, method="overlap-save", B=512, h=None, partition=None):    
         self.method = method
         self.B = B                # block size (audio input len, which will also be the output size)
         self.NFFT = None          # fft/ifft size
-        self.flagIRchanged = True # check if the IR changed or it's still the same
+        self.flagIRchanged = True  # check if the IR changed or it's still the same
         self.stored_h = h         # save IR for comparison next frame (optional input)
-        self.left_overs = np.zeros((B,)) # remaining samples from last ifft (OLA)
-        self.partition = partition # partition size (UPOLS)       
-    
-                
+        self.left_overs = np.zeros((B,))  # remaining samples from last ifft (OLA)
+        self.partition = partition  # partition size (UPOLS)
 
     def pad_the_end(self, x, new_length):
         output = np.zeros((new_length,))
         output[:x.shape[0]] = x
         return output
-    
+
     def pad_beginning(self, x, padding):
         new_length = max(x.shape) + padding
         output = np.zeros((new_length,))
@@ -74,27 +72,25 @@ class FIRfilter():
             self.flagIRchanged = False
         return ifft(X * self.H).real 
 
-
     def OLA(self, x, h):
         '''Overlap-add convolution'''
         if self.NFFT is None:
             Nx = max(x.shape)
             Nh = max(h.shape)
             self.NFFT = self.next_power_of_2(Nx + Nh - 1)  
-                 
+
         # Fast convolution
         y = self.fft_conv(x, h)
-        
+
         # Overlap-Add the partial convolution result        
         out = y[:self.B] + self.left_overs[:self.B]
-        
-        self.left_overs = np.delete(self.left_overs, [range(0,self.B)], 0)    
+
+        self.left_overs = np.delete(self.left_overs, [range(0, self.B)], 0)    
         pad = len(y[self.B+1:])
-        self.left_overs = self.pad_the_end(self.left_overs,pad) + y[self.B+1:] 
-        if len(self.left_overs) < self.B: # fix size for next loop (if needed)
+        self.left_overs = self.pad_the_end(self.left_overs, pad) + y[self.B+1:] 
+        if len(self.left_overs) < self.B:  # fix size for next loop (if needed)
             self.left_overs = self.pad_the_end(self.left_overs, self.B)      
         return out
-
 
     def OLS(self, x, h):
         '''Overlap-save convolution'''
@@ -104,39 +100,37 @@ class FIRfilter():
             self.NFFT = self.next_power_of_2(Nx + Nh - 1) 
             # Input buffer
             self.OLS_buffer = np.zeros(shape=(self.NFFT,))        
-        
+
         # Sliding window of the input
-        self.OLS_buffer = np.roll(self.OLS_buffer, -self.B) #previous contents are shifted B samples to the left
-        self.OLS_buffer[self.B:2*self.B] = x #next length-B input block is stored rightmost
+        self.OLS_buffer = np.roll(self.OLS_buffer, -self.B)  # previous contents are shifted B samples to the left
+        self.OLS_buffer[self.B:2*self.B] = x  # next length-B input block is stored rightmost
 
         # Fast convolution
         y = self.fft_conv(self.OLS_buffer, h)     
         return y[-self.B:]
 
-
-    def cost(B,N,L,K):
-        '''theoretical time estimates for each operation, pag. 211'''
-        # def T_fft(n):
-        #     return 1.68 *n*np.log2(n)
-        # def T_ifft(n):
-        #     return 3.49 *n*np.log2(n)
-        # def T_cmul(n):
-        #     return 6*n
-        # def T_cmac(n):
-        #     return 8*n   
-        # return 1/B*(T_fft(K) + T_ifft(K) + T_cmul((K+1)/2) + ((N/L)-1)*T_cmac((K+1)/2))
-        return 1/B*( 1.68*K*np.log2(K) + 3.49*K*np.log2(K) + 6*((K+1)/2) + ((N/L)-1)*8*((K+1)/2))
-        
-    
     def optimize_UPOLS_parameters(self, N, B):
         '''brute-force the optimal parameters for UPOLS
             N: IR length
             B: buffer size
-        '''            
+        '''
+        def cost(B, N, L, K):
+            '''theoretical time estimates for each operation, pag. 211'''
+            # def T_fft(n):
+            #     return 1.68 *n*np.log2(n)
+            # def T_ifft(n):
+            #     return 3.49 *n*np.log2(n)
+            # def T_cmul(n):
+            #     return 6*n
+            # def T_cmac(n):
+            #     return 8*n   
+            # return 1/B*(T_fft(K) + T_ifft(K) + T_cmul((K+1)/2) + ((N/L)-1)*T_cmac((K+1)/2))
+            return 1/B*(1.68*K*np.log2(K) + 3.49*K*np.log2(K) + 6*((K+1)/2) + ((N/L)-1)*8*((K+1)/2))
+        
         c_opt = Inf
         rang = np.array([2**k for k in range(0, int(np.log2(N)))]).astype('int')
         for L in rang:
-            d_max = B - np.gcd(L,B)
+            d_max = B - np.gcd(L, B)
             K_min = int(B + L + d_max - 1)
             K_max = int(2**np.ceil(np.log2(K_min)))
             k_sort = np.sort([K_min, K_max])
@@ -144,15 +138,15 @@ class FIRfilter():
                 k_sort[1] = k_sort[1]+1
             
             for K in range(k_sort[0], k_sort[1]):
-                c = self.cost(B,N,L,K)
+                c = cost(B, N, L, K)
                 if c < c_opt:
                     c_opt = c
                     L_opt = L
                     K_opt = K
         return L_opt, K_opt
-        
-    
-    def UPOLS(self,x,h):
+
+
+    def UPOLS(self, x, h):
         '''(generalized) Uniformly Partitioned Overlap-Save
             - generalized means that you can pick the partition size
         '''
@@ -163,40 +157,40 @@ class FIRfilter():
                 self.partition, self.NFFT = self.optimize_UPOLS_parameters(Nh, self.B)
                 L_partit = self.partition
             else:
-                L_partit = self.partition              
-                dmax = self.B - np.gcd(L_partit, self.B)           
-                self.NFFT = self.B + L_partit + dmax 
-            
-            self.P = int(np.ceil(Nh/L_partit)) # number of partitions done          
-            self.input_buffer = np.zeros(shape=(self.NFFT,))   # Initialize input buffer              
+                L_partit = self.partition
+                dmax = self.B - np.gcd(L_partit, self.B)  
+                self.NFFT = self.B + L_partit + dmax
+
+            self.P = int(np.ceil(Nh/L_partit))  # number of partitions done
+            self.input_buffer = np.zeros(shape=(self.NFFT,))  # Initialize input buffer
             # Initialize filter and FDL
-            self.nm = np.zeros((self.P,)) # tells us which FDL positions should be used 
-            self.H = np.zeros((self.P, self.NFFT),dtype='complex_') # partitioned filters (freq domain)
-            
-            # (1) split original filter into P length-L sub filters  
+            self.nm = np.zeros((self.P,))  # tells us which FDL positions should be used
+            self.H = np.zeros((self.P, self.NFFT), dtype='complex_')  # partitioned filters (freq domain)
+
+            # (1) split original filter into P length-L sub filters
             for m, ii in enumerate(range(0, Nh, L_partit)):
                 try:
                     h_partit = h[ii:ii+L_partit]
                 except:
                     h_partit = self.pad_the_end(h[ii:], L_partit)
-                    
-                # (2) incorporate "remainder delays"         
-                dm = np.mod(m*L_partit, self.B)         
+
+                # (2) incorporate "remainder delays"
+                dm = np.mod(m*L_partit, self.B)
                 h_pad = self.pad_beginning(h_partit, dm)
-                self.H[m,:] = fft(h_pad, n=self.NFFT)    
-                self.nm[m] = np.floor(m*L_partit/self.B)  # FDL active slots        
+                self.H[m, :] = fft(h_pad, n=self.NFFT)
+                self.nm[m] = np.floor(m*L_partit/self.B)  # FDL active slots
             self.nm = self.nm.astype(int)
-            self.FDL = np.zeros((max(self.nm)+1, self.NFFT), dtype='complex_')  # delay line 
-            
+            self.FDL = np.zeros((max(self.nm)+1, self.NFFT), dtype='complex_')  # delay line
+
         # (3) Sliding window of the input
         self.input_buffer = np.roll(self.input_buffer, shift=-self.B) #previous contents are shifted B samples to the left
-        self.input_buffer[-self.B:] = x #next length-B input block is stored rightmost     
+        self.input_buffer[-self.B:] = x  # next length-B input block is stored rightmost     
         # (4) Stream 
-        self.FDL = np.roll(self.FDL, shift=1, axis=0) # shift the FDL to create space for current input  
-        self.FDL[0,:] = fft(self.input_buffer) # add current buffer to the first FDL slot
+        self.FDL = np.roll(self.FDL, shift=1, axis=0)  # shift the FDL to create space for current input
+        self.FDL[0, :] = fft(self.input_buffer)  # add current buffer to the first FDL slot
         # convo
         # note: the sum is done in the frequency domain (yep!)
-        out = ifft(np.sum(np.multiply(self.FDL[self.nm,:], self.H), axis=0)).real
+        out = ifft(np.sum(np.multiply(self.FDL[self.nm, :], self.H), axis=0)).real
         return out[-self.B:]
     
     
