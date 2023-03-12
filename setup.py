bda369227dd6dbb6e2203d3b6057aac00195fb73
Elephant ~ <r.davicarvalho@gmail.com>
2023-03-12 17:52:45 +0000

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="FIRconv",
+    description="Python implementation of real-time convolution for auralization",
+    author="Davi Carvalho",
+    url="https://github.com/davircarvalho/FIRconv",
+    author_email="r.davicarvalho@gmail.com",
+    version="0.0.1",
+    install_requires=[
+        "numpy"
+        #"librosa",
+        #"pyaudio",
+        #"matplotlib"
+    ],
+    packages=find_namespace_packages(include='FIRconv.*'),
+    package_data={}, # No data yet.
+    # extras_require={
+    #     "dev": ["flake8", "flake8-black"],
+    #     "test": ["pytest"],
+    # },
+    entry_points={}, # No executable scripts yet
+)
